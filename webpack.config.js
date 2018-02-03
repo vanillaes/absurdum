a633023ad0b6d733e0746a2ecf27862e11f5bc12
Evan Plaice <evanplaice@gmail.com>
2018-02-03 06:00:42 -0700

    Add webpack + config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    'arrays': './src/arrays/index.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [ 'env' ]
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: '[name].js',
+    libraryTarget: 'umd'
+  }
+};
