25696b9a4da4621bde1c1fd375520ab34e74fa31
Evan Plaice <evanplaice@gmail.com>
2018-02-14 08:20:40 -0700

    Add endsWith

--- docs/strings.md
+++ docs/strings.md
@@ -2,6 +2,22 @@
 
 ## Operators
 
+### strings.endsWith(string, substr)
+
+EndsWith tests a string to see if it ends with a substring
+
+```javascript
+let result = strings.endsWith('This sentence ends with', 'with');
+console.log(result);
+> true
+```
+
+```javascript
+let result = strings.endsWith('This sentence does not end with', 'nope');
+console.log(result);
+> false
+```
+
 ### strings.startsWith(string, substr)
 
 StartsWith tests a string to see if it starts with a substring
--- test/strings.js
+++ test/strings.js
@@ -13,3 +13,16 @@ test('strings.startsWith(array) - checks to see if the input starts with a strin
   t.equal(result, expect, 'value mismatch');
   t.end();
 });
+
+test('strings.endsWith(array) - checks to see if the input ends with a string', (t) => {
+  let result = strings.endsWith('This sentence ends with', 'with');
+  let expect = true;
+  t.equal(Object.prototype.toString.call(result), '[object Boolean]', 'type');
+  t.equal(result, expect, 'value match');
+
+  result = strings.endsWith('This sentence does not end with', 'nope');
+  expect = false;
+  t.equal(Object.prototype.toString.call(result), '[object Boolean]', 'type');
+  t.equal(result, expect, 'value mismatch');
+  t.end();
+});
