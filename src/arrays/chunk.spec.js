4b4ecb2b15eed82b004f39333240b9be3b5c8496
Evan Plaice <evanplaice@gmail.com>
2019-08-06 21:41:08 -0600

    Add missing arrays.chunk test
    Forgot to uncomment the test during development.

--- src/arrays/chunk.spec.js
+++ src/arrays/chunk.spec.js
@@ -1,16 +1,16 @@
 import test from 'tape';
 import { arrays } from '../../index.js';
 
-// test(`arrays.chunk(array) - should return a chunk for each item in the array`, t => {
-//   const expect = [[1], [2], [3], [4]];
-//   const result = arrays.chunk([1, 2, 3, 4]);
+test(`arrays.chunk(array) - should return a chunk for each item in the array`, t => {
+  const expect = [[1], [2], [3], [4]];
+  const result = arrays.chunk([1, 2, 3, 4]);
 
-//   t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
-//   t.equal(result.length, 4, 'output length');
-//   t.deepEqual(result, expect, 'output value');
+  t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
+  t.equal(result.length, 4, 'output length');
+  t.deepEqual(result, expect, 'output value');
 
-//   t.end();
-// });
+  t.end();
+});
 
 test(`arrays.chunk(array, size) - should return an array of chunks of the specified size`, t => {
   const expect = [[1, 2], [3, 4], [5]];
