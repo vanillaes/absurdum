4d85f2dfd0cac2fbd486234ee7f442e9596071dd
Evan Plaice <evanplaice@gmail.com>
2019-10-16 16:20:46 -0600

    Add CJS compatibility test

---
+++ compat/chunk.spec.cjs
@@ -0,0 +1,24 @@
+const test = require('tape');
+const arrays = require('../dist/absurdum.cjs.js').arrays;
+
+test('arrays.chunk(array) - should return a chunk for each item in the array', t => {
+  const expect = [[1], [2], [3], [4]];
+  const result = arrays.chunk([1, 2, 3, 4]);
+
+  t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
+  t.equal(result.length, 4, 'output length');
+  t.deepEqual(result, expect, 'output value');
+
+  t.end();
+});
+
+test('arrays.chunk(array, size) - should return an array of chunks of the specified size', t => {
+  const expect = [[1, 2], [3, 4], [5]];
+  const result = arrays.chunk([1, 2, 3, 4, 5], 2);
+
+  t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
+  t.equal(result.length, 3, 'output length');
+  t.deepEqual(result, expect, 'output value');
+
+  t.end();
+});
