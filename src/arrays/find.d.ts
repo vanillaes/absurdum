12b2bf826f1faa5d69ea3df26fed23d4f13d47bd
jkvyff <kristianvyff@gmail.com>
2019-11-04 19:39:25 -0800

    added find.d.ts info

--- src/arrays/find.d.ts
+++ src/arrays/find.d.ts
@@ -1,1 +1,17 @@
-find.d.ts
+/**
+ * Find method returns the value of first element at which a provided function is true, 
+ * or undefined if no elements in the array satisfy the function.
+ *
+ * @param {Array} array
+ * @param {Function} predicate to be run against each element of the array
+ * @param {*} value of this 
+ * @returns {*} value of element that satisfied function.
+ * @example
+ * const result = arrays.find([5, 12, 8, 130, 44], (x) => x > 10);
+ * console.log(result);
+ * > 12
+ * @example2
+ * const result = arrays.find([5, 12, 8, 130, 44], function(x) { return x > this }, 4);
+ * console.log(result);
+ * > 5
+ */
