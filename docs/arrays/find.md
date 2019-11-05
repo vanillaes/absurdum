065424f5350ee01ea2c0ea55d38f4da72d629585
jkvyff <kristianvyff@gmail.com>
2019-11-05 15:27:29 -0800

    add array find

---
+++ docs/arrays/find.md
@@ -0,0 +1,35 @@
+# arrays.find
+
+<!-- div class="doc-container" -->
+
+<!-- div -->
+
+
+<!-- div -->
+
+<h3 id="findarray-predicate-value"><code>find(array, predicate, value)</code></h3>
+
+Find method returns the value of first element at which a provided function is true, 
+or undefined if no elements in the array satisfy the function.
+
+#### Arguments
+1. `array` *(Array)*:
+2. `predicate` *(Function)*: to be run against each element of the array
+3. `value` *(&#42;)*: of this
+
+#### Returns
+*(&#42;)*: value of element that satisfied function.
+
+#### Example
+```js
+const result = arrays.find([5, 12, 8, 130, 44], (x) => x > 10);
+console.log(result);
+> 12
+```
+---
+
+<!-- /div -->
+
+<!-- /div -->
+
+<!-- /div -->
---
+++ src/arrays/find.spec.js
@@ -0,0 +1,52 @@
+import test from 'tape';
+import { arrays } from '../../index.js';
+
+test('arrays.find(array, predicate) - should return the value of first element at which a provided function is true', t => {
+  const expect = 12;
+  const result = arrays.find([5, 12, 8, 130, 44], (x) => x > 10);
+
+  t.equal(Object.prototype.toString.call(result), '[object Number]', 'return type');
+  t.equal(result, expect, 'output value');
+
+  t.end();
+});
+
+test('arrays.find(array, predicate) - should return the value of first element at which a provided function is true', t => {
+  const expect = 'hello';
+  const result = arrays.find(['hello'], (x) => x === 'hello');
+
+  t.equal(Object.prototype.toString.call(result), '[object String]', 'return type');
+  t.equal(result, expect, 'output value');
+
+  t.end();
+});
+
+test('arrays.find(array, predicate, thisArg) - should return the value of first element at which a provided function is true, with a replaceable this', t => {
+  const expect = 5;
+  const result = arrays.find([5, 12, 8, 130, 44], function(x) { return x > this }, 4);
+
+  t.equal(Object.prototype.toString.call(result), '[object Number]', 'return type');
+  t.equal(result, expect, 'output value');
+
+  t.end();
+});
+
+test('arrays.find(array, predicate) - should return undefined if the array is empty', t => {
+  const expect = undefined;
+  const result = arrays.find([], (x) => x > 10);
+
+  t.equal(Object.prototype.toString.call(result), '[object Undefined]', 'return type');
+  t.equal(result, expect, 'output value');
+
+  t.end();
+});
+
+test('arrays.find(array, predicate) - should return undefined if the array no element in the array satisfies the function', t => {
+  const expect = undefined;
+  const result = arrays.find([5, 12, 8, 130, 44], (x) => x > 200);
+
+  t.equal(Object.prototype.toString.call(result), '[object Undefined]', 'return type');
+  t.equal(result, expect, 'output value');
+
+  t.end();
+});
