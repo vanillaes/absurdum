f3b2ed563af46d4a9f1cb8eaea0959fa3607909f
Evan Plaice <evanplaice@gmail.com>
2019-11-05 17:01:14 -0700

    Add missing typings

---
+++ src/arrays/find.d.ts
@@ -0,0 +1,18 @@
+/**
+ * Find method returns the value of first element at which a provided function is true,
+ * or undefined if no elements in the array satisfy the function.
+ *
+ * @param {Array} array
+ * @param {Function} predicate to be run against each element of the array
+ * @param {*} thisArg of this
+ * @returns {*} value of element that satisfied function.
+ * @example
+ * const result = arrays.find([5, 12, 8, 130, 44], (x) => x > 10);
+ * console.log(result);
+ * > 12
+ * @example
+ * const result = arrays.find([5, 12, 8, 130, 44], function(x) { return x > this }, 4);
+ * console.log(result);
+ * > 5
+ */
+export function find(array: any[], predicate: Function, thisArg?: any): any;
