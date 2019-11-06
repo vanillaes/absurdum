00382bb46761bdc9e7b0b70fcca2a32034a9e7f9
Evan Plaice <evanplaice@gmail.com>
2019-11-05 17:07:16 -0700

    Cleanup
    Prepare to publish

--- src/arrays/every.js
+++ src/arrays/every.js
@@ -4,7 +4,7 @@
  *
  * @param {Array} array
  * @param {Function} predicate to be run against each element of the array
- * @param {*} value of this 
+ * @param {*} thisArg of this
  * @returns {Boolean} if all elements in array satisfy function.
  * @example
  * const result = arrays.every(["ham", "cheese", "bread"], (x) => x.length >= 3);
@@ -17,18 +17,16 @@
  */
 function every (array, predicate, thisArg = undefined) {
   if (array.length === 0) return false;
-  if (this == null) throw TypeError('"this" is null or not defined');
-  if (typeof predicate !== 'function') {
-    throw TypeError('predicate must be a function');
-  };
+  if (this == null) { throw TypeError('"this" is null or not defined'); }
+  if (typeof predicate !== 'function') { throw TypeError('predicate must be a function'); }
   if (!predicate.call(thisArg, array[0])) return false;
   if (array.length === 1) return true;
 
-  return array.reduce((res, cur, i) => {     
+  return array.reduce((res, cur, i) => {
     if (i === 1) return predicate.call(thisArg, cur);
     if (res && predicate.call(thisArg, cur)) return true;
     return false;
   });
-};
+}
 
 export { every };
--- src/arrays/every.spec.js
+++ src/arrays/every.spec.js
@@ -3,7 +3,7 @@ import { arrays } from '../../index.js';
 
 test('arrays.every(array, predicate) - should return true if all values return true for the function', t => {
   const expect = true;
-  const result = arrays.every(["ham", "cheese", "bread"], (x) => x.length >= 3);
+  const result = arrays.every(['ham', 'cheese', 'bread'], (x) => x.length >= 3);
 
   t.equal(Object.prototype.toString.call(result), '[object Boolean]', 'return type');
   t.equal(result, expect, 'output value');
@@ -33,7 +33,7 @@ test('arrays.every(array, predicate) - should return false if array is empty', t
 
 test('arrays.every(array, predicate, thisArg) - should return false if any value does not return true for the function, with a replaceable this', t => {
   const expect = false;
-  const result = arrays.every(["ham", "cheese", "bread"], function(x) { return x.length > this }, 4);
+  const result = arrays.every(['ham', 'cheese', 'bread'], function (x) { return x.length > this; }, 4);
 
   t.equal(Object.prototype.toString.call(result), '[object Boolean]', 'return type');
   t.equal(result, expect, 'output value');
@@ -43,7 +43,7 @@ test('arrays.every(array, predicate, thisArg) - should return false if any value
 
 test('arrays.every(array, predicate, thisArg) - should return true if every value in the array satisfies the function, with a replaceable this', t => {
   const expect = true;
-  const result = arrays.every(["cheese", "cheese", "cheese"], function(x) { return x === this }, "cheese");
+  const result = arrays.every(['cheese', 'cheese', 'cheese'], function (x) { return x === this; }, 'cheese');
 
   t.equal(Object.prototype.toString.call(result), '[object Boolean]', 'return type');
   t.equal(result, expect, 'output value');
