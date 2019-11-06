710bb230f80be7458f97eb6d9422f8b6fb264ceb
Evan Plaice <evanplaice@gmail.com>
2019-11-05 17:07:41 -0700

    Build

--- dist/absurdum.esm.js
+++ dist/absurdum.esm.js
@@ -150,6 +150,37 @@ function dropRight (array, n = 1) {
 }
 
 /**
+ * Every method returns true if every element in the array satisfies the function
+ * and false if any element in the array does not satisfy the function.
+ *
+ * @param {Array} array
+ * @param {Function} predicate to be run against each element of the array
+ * @param {*} thisArg of this
+ * @returns {Boolean} if all elements in array satisfy function.
+ * @example
+ * const result = arrays.every(["ham", "cheese", "bread"], (x) => x.length >= 3);
+ * console.log(result);
+ * > true
+ * @example
+ * const result = arrays.every(["ham", "cheese", "bread"], function(x) { return x.length >= this }, 3);
+ * console.log(result);
+ * > true
+ */
+function every (array, predicate, thisArg = undefined) {
+  if (array.length === 0) return false;
+  if (this == null) { throw TypeError('"this" is null or not defined'); }
+  if (typeof predicate !== 'function') { throw TypeError('predicate must be a function'); }
+  if (!predicate.call(thisArg, array[0])) return false;
+  if (array.length === 1) return true;
+
+  return array.reduce((res, cur, i) => {
+    if (i === 1) return predicate.call(thisArg, cur);
+    if (res && predicate.call(thisArg, cur)) return true;
+    return false;
+  });
+}
+
+/**
  * Fills items in an array with a specified value. (Optional) can start and/or end from a specific index.
  *
  * @param {Array} array input array
@@ -348,6 +379,7 @@ var index = /*#__PURE__*/Object.freeze({
   difference: difference,
   drop: drop,
   dropRight: dropRight,
+  every: every,
   fill: fill,
   filter: filter,
   find: find,
