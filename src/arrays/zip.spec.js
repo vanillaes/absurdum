1460e68886f1c31a5358aca976f12fa3619d004c
Evan Plaice <evanplaice@gmail.com>
2019-11-06 15:25:33 -0700

    Cleanup
    Prepare to publish

--- src/arrays/zip.spec.js
+++ src/arrays/zip.spec.js
@@ -2,8 +2,8 @@ import test from 'tape';
 import { arrays } from '../../index.js';
 
 test('arrays.zip(array1, array2) - should return an array with arrays of corresponding values', t => {
-  const expect = [ [5, 'ham'], [12, 'cheese'], [8, 'bread'] ];
-  const result = arrays.zip([5, 12, 8], ["ham", "cheese", "bread"]);
+  const expect = [[5, 'ham'], [12, 'cheese'], [8, 'bread']];
+  const result = arrays.zip([5, 12, 8], ['ham', 'cheese', 'bread']);
 
   t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
   t.equal(result.length, 3, 'output length');
@@ -13,8 +13,8 @@ test('arrays.zip(array1, array2) - should return an array with arrays of corresp
 });
 
 test('arrays.zip(array1, array2) - should return an array with arrays of corresponding values, the length of the shortest array', t => {
-  const expect = [ [5, 'ham'], [12, 'cheese'], [8, 'bread'] ];
-  const result = arrays.zip([5, 12, 8, 130, 44], ["ham", "cheese", "bread"]);
+  const expect = [[5, 'ham'], [12, 'cheese'], [8, 'bread']];
+  const result = arrays.zip([5, 12, 8, 130, 44], ['ham', 'cheese', 'bread']);
 
   t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
   t.equal(result.length, 3, 'output length');
@@ -26,7 +26,7 @@ test('arrays.zip(array1, array2) - should return an array with arrays of corresp
 
 test('arrays.zip(array1, array2) - should return an empty array if either of the input arrays are empty', t => {
   const expect = [];
-  const result = arrays.zip([], ["ham", "cheese", "bread"]);
+  const result = arrays.zip([], ['ham', 'cheese', 'bread']);
 
   t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
   t.equal(result.length, 0, 'output length');
@@ -36,8 +36,8 @@ test('arrays.zip(array1, array2) - should return an empty array if either of the
 });
 
 test('arrays.zip(array1, array2, predicate) - should return an array with arrays of corresponding values, with the predicate applied to value pairs', t => {
-  const expect = [ '15 ham', '12 cheese', '18 bread' ];
-  const result = arrays.zip([15, 12, 18, 40, 30],["ham", "cheese", "bread"], (x, y) => x + " " + y);
+  const expect = ['15 ham', '12 cheese', '18 bread'];
+  const result = arrays.zip([15, 12, 18, 40, 30], ['ham', 'cheese', 'bread'], (x, y) => x + ' ' + y);
 
   t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
   t.equal(result.length, 3, 'output length');
@@ -48,8 +48,8 @@ test('arrays.zip(array1, array2, predicate) - should return an array with arrays
 });
 
 test('arrays.zip(array1, array2, predicate) - should return an array with arrays of corresponding values, with the predicate applied to value pairs', t => {
-  const expect = [ { x: 15, y: 'ham' }, { x: 12, y: 'cheese' }, { x: 18, y: 'bread' } ];
-  const result = arrays.zip([15, 12, 18, 40, 30],["ham", "cheese", "bread"], (x, y) => ({x, y}));
+  const expect = [{ x: 15, y: 'ham' }, { x: 12, y: 'cheese' }, { x: 18, y: 'bread' }];
+  const result = arrays.zip([15, 12, 18, 40, 30], ['ham', 'cheese', 'bread'], (x, y) => ({ x, y }));
 
   t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
   t.equal(result.length, 3, 'output length');
