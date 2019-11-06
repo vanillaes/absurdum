45837f5da164125f7e4549498a8110ef34219012
Evan Plaice <evanplaice@gmail.com>
2019-11-06 15:26:37 -0700

    Build

--- dist/absurdum.cjs.js
+++ dist/absurdum.cjs.js
@@ -396,6 +396,41 @@ function tap (array, func) {
   return array;
 }
 
+/**
+ * Zip applies a specified function to the corresponding elements of two sequences,
+ * producing a sequence of the results.
+ *
+ * @param {Array} array1 input array
+ * @param {Array} array2 input array
+ * @param {Function} predicate optional to be applied to corresponding values
+ * @returns {Array} input array filled value pairs after the function has been applied
+ *
+ * @example
+ * const result = zip([5, 12, 8, 130, 44], ["ham", "cheese", "bread"]);
+ * console.log(result)
+ * > [ [ 'ham', 5 ], [ 'cheese', 12 ], [ 'bread', 8 ] ]
+ * @example
+ * const result = zip([5, 12, 8], ["ham", "cheese", "bread"], (x, y) => x + " " + y);
+ * console.log(result)
+ * > [ 'ham 5', 'cheese 12', 'bread 8' ]
+ */
+function zip (array1, array2, predicate = (a, b) => [a, b]) {
+  if (predicate && typeof predicate !== 'function') {
+    throw TypeError('predicate must be a function');
+  }
+  if (array1.length <= array2.length) {
+    return array1.reduce((res, cur, i) => {
+      res[i] = predicate(cur, array2[i]);
+      return res;
+    }, []);
+  } else {
+    return array2.reduce((res, cur, i) => {
+      res[i] = predicate(array1[i], cur);
+      return res;
+    }, []);
+  }
+}
+
 
 
 var index = /*#__PURE__*/Object.freeze({
@@ -415,7 +450,8 @@ var index = /*#__PURE__*/Object.freeze({
   map: map,
   reduceRight: reduceRight,
   reverse: reverse,
-  tap: tap
+  tap: tap,
+  zip: zip
 });
 
 /**
