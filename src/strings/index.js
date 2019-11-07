872d28e59ae73fb462bba2dd37c3e4a4747859c8
jkvyff <kristianvyff@gmail.com>
2019-11-07 15:16:19 -0800

    added string includes

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,4 +1,5 @@
 export { endsWith } from './endsWith.js';
+export { includes } from './includes.js';
 export { padEnd } from './padEnd.js';
 export { padStart } from './padStart.js';
 export { reverse } from './reverse.js';
