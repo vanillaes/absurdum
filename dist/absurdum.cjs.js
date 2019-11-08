7eea3ab87201eac6b7c84ffabd155ce86cc70f13
Evan Plaice <evanplaice@gmail.com>
2019-11-07 20:59:21 -0700

    Build

--- dist/absurdum.cjs.js
+++ dist/absurdum.cjs.js
@@ -490,6 +490,40 @@ function endsWith (string, substr = '') {
 }
 
 /**
+ * Includes determines whether one string can be found in another string
+ *
+ * @param {string} string input string
+ * @param {string} substr candidate string to be searched for
+ * @param {Number} start optional index to begin search for string
+ * @returns {boolean} does the input string include the substring?
+ *
+ * @example
+ * const result = strings.includes('This Lovely Life', 'Love');
+ * console.log(result);
+ * > true
+ * @example
+ * const result = strings.includes('This Lovely Life', 'Love', 5);
+ * console.log(result);
+ * > false
+ */
+function includes (string, substr, start = 0) {
+  if (string.length === 0) return false;
+  const len = substr.length;
+  const first = substr.charAt(0);
+  if (start === 0 && string.substring(0, len) === substr) return true;
+
+  return string.split('').reduce((res, cur, i) => {
+    if (i >= start) {
+      if (res) return res;
+      if (cur === first) {
+        return string.substring(i, i + len) === substr;
+      }
+    }
+    return false;
+  }, false);
+}
+
+/**
  * Pads the end of a string w/ repeated spaces|substrings
  *
  * @param {string} string input string
@@ -639,6 +673,7 @@ function startsWith (string, substr) {
 var index$1 = /*#__PURE__*/Object.freeze({
   __proto__: null,
   endsWith: endsWith,
+  includes: includes,
   padEnd: padEnd,
   padStart: padStart,
   reverse: reverse$1,
