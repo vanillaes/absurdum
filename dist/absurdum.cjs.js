e53230171a8f021fe25299e9b5cd148993899da0
Evan Plaice <evanplaice@gmail.com>
2019-11-10 12:16:42 -0700

    Build

--- dist/absurdum.cjs.js
+++ dist/absurdum.cjs.js
@@ -376,6 +376,38 @@ function reverse (array) {
 }
 
 /**
+ * Some method returns true if any element in the array satisfies the function
+ * and false if no element in the array satisfies the function.
+ *
+ * @param {Array} array input array
+ * @param {Function} predicate function to be run against each element of the array
+ * @param {*} [thisArg=undefined] A value to use as this when executing predicate.
+ * @returns {Boolean} if any of the elements in array satisfy the function.
+ * @example
+ * const result = arrays.some(["ham", "cheese", "bread"], (x) => x.length === 3);
+ * console.log(result);
+ * > true
+ * @example
+ * const result = arrays.some(["ham", "cheese", "bread"], function(x) { return x.length === this }, 3);
+ * console.log(result);
+ * > true
+ */
+function some (array, predicate, thisArg = undefined) {
+  if (array.length === 0) return false;
+  if (this == null) { throw TypeError('"this" is null or not defined'); }
+  if (typeof predicate !== 'function') { throw TypeError('predicate must be a function'); }
+  if (predicate.call(thisArg, array[0])) return true;
+  if (array.length === 1) return true;
+
+  return array.reduce((res, cur, i, arr) => {
+    if (res === true) { return true; }
+    if (i === 1) return predicate.call(thisArg, cur);
+    if (predicate.call(thisArg, cur)) return true;
+    return false;
+  });
+}
+
+/**
  * Applies a function to each element in the array without mutating it
  *
  * @param {Array} array input array
@@ -450,6 +482,7 @@ var index = /*#__PURE__*/Object.freeze({
   map: map,
   reduceRight: reduceRight,
   reverse: reverse,
+  some: some,
   tap: tap,
   zip: zip
 });
