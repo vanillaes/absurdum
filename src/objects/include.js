e487248ce7ec56dd1ac429ba24de96c66a95c046
jkvyff <kristianvyff@gmail.com>
2019-11-13 08:10:38 -0800

    add objects.include

---
+++ src/objects/include.js
@@ -0,0 +1,25 @@
+/**
+ * Include determines whether one string can be found in another string
+ *
+ * @param {object} object input string
+ * @param {array} filter array of keys to be kept in the filtered object
+ * @returns {object} object filtered to only include elemnts with a key from the filter
+ *
+ * @example
+ * const result = objects.include({ small: "ant", medium: "dog", big: "elephant" }, ['small', 'big']);
+ * console.log(result);
+ * > { small: 'ant', big: 'elephant' }
+ */
+function include(object, filter) {
+    return Object.entries(object).reduce((acc, curr, i) => {
+        if (filter.reduce((res, cur) => {
+            if (res === true || cur === curr[0]) return true;
+            return false;
+        }, "")) {
+            acc[curr[0]] = curr[1];
+        }
+        return acc;
+    }, {});
+};
+
+export { include };
---
+++ src/objects/include.spec.js
@@ -0,0 +1,52 @@
+import test from 'tape';
+import { objects } from '../../index.js';
+
+test('objects.include(object, filter) - returns an object filtered by the filter array', t => {
+    const expect = { small: "ant", big: "elephant" };
+    const result = objects.include({ small: "ant", medium: "dog", big: "elephant" }, ['small', 'big']);
+
+    t.equal(Object.prototype.toString.call(result), '[object Object]', 'return type');
+    t.deepEqual(result, expect, 'output value');
+
+    t.end();
+});
+
+test('objects.include(object, filter) - returns an object filtered by the filter array, ignores unused elements of the filter array', t => {
+    const expect = { small: "ant", big: "elephant" };
+    const result = objects.include({ small: "ant", medium: "dog", big: "elephant" }, ['small', 'big', 'enormous']);
+
+    t.equal(Object.prototype.toString.call(result), '[object Object]', 'return type');
+    t.deepEqual(result, expect, 'output value');
+
+    t.end();
+});
+
+test('objects.include(object, filter) - returns an empty object if no elments in the filter array exist as object keys', t => {
+    const expect = {};
+    const result = objects.include({ small: "ant", medium: "dog", big: "elephant" }, ['tiny', 'mini']);
+
+    t.equal(Object.prototype.toString.call(result), '[object Object]', 'return type');
+    t.deepEqual(result, expect, 'output value');
+
+    t.end();
+});
+
+test('objects.include(object, filter) - returns an empty object if the input object is empty', t => {
+    const expect = {};
+    const result = objects.include({}, ['small', 'big']);
+
+    t.equal(Object.prototype.toString.call(result), '[object Object]', 'return type');
+    t.deepEqual(result, expect, 'output value');
+
+    t.end();
+});
+
+test('objects.include(object, filter) - should not mutate the input', t => {
+    const input = { small: "ant", medium: "dog", big: "elephant" };
+    const expect = { small: "ant", medium: "dog", big: "elephant" };
+    objects.include(input, ['small', 'big']);
+
+    t.deepEqual(input, expect, 'input mutation');
+
+    t.end();
+});
