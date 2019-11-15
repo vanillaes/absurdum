0ed2736a840a07111376d890ef5b66200e979a0f
jkvyff <kristianvyff@gmail.com>
2019-11-14 21:12:41 -0800

    add objects.exclude

---
+++ src/objects/exclude.js
@@ -0,0 +1,25 @@
+/**
+ * Exclude filters out elements from an object based on an array of keys to exclude
+ *
+ * @param {object} object input string
+ * @param {array} filter array of keys to be excluded in the filtered object
+ * @returns {object} object filtered to exclude elemnts with a key from the filter
+ *
+ * @example
+ * const result = objects.exclude({ small: 'ant', medium: 'dog', big: 'elephant' }, ['small', 'big']);
+ * console.log(result);
+ * > { medium: "dog" }
+ */
+function exclude(object, filter) {
+    return Object.entries(object).reduce((acc, curr, i) => {
+        if (filter.reduce((res, cur) => {
+            if (res === false || cur === curr[0]) { return false; }
+            return true;
+        }, '')) {
+            acc[curr[0]] = curr[1];
+        }
+        return acc;
+    }, {});
+}
+
+export { exclude };
