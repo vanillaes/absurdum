12ebf02fd4cad14d3b4cbfcfba3dfc052423bdd2
jkvyff <kristianvyff@gmail.com>
2019-11-15 19:17:50 -0800

    changed so concat combines elements of arrays without flattening

--- src/arrays/concat.spec.js
+++ src/arrays/concat.spec.js
@@ -1,12 +1,34 @@
 import test from 'tape';
 import { arrays } from '../../index.js';
 
-test('arrays.concat(...arrays) - should return an array with all inputs flattened and concatenated', t => {
-  const expect = [1, 2, 3, 4];
+test('arrays.concat(...arrays) - should return an array with all inputs concatenated', t => {
+  const expect = ['Hello', 'Greetings', 'Farewell', 'Goodbye'];
+  const result = arrays.concat(['Hello', 'Greetings'], ['Farewell', 'Goodbye']);
+
+  t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
+  t.equal(result.length, 4, 'output length');
+  t.deepEqual(result, expect, 'output value');
+
+  t.end();
+});
+
+test('arrays.concat(...arrays) - should return an array with all inputs concatenated', t => {
+  const expect = [1, 2, 3, [4]];
   const result = arrays.concat([1], 2, [3], [[4]]);
 
   t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
   t.equal(result.length, 4, 'output length');
+  t.deepEqual(result, expect, 'output value');
+
+  t.end();
+});
+
+test('arrays.concat(...arrays) - should return an array with all inputs concatenated', t => {
+  const expect = ['Tao', 2, 'Apolline', [3], 5];
+  const result = arrays.concat(['Tao'], 2, ['Apolline', [3]], [5]);
+
+  t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
+  t.equal(result.length, 5, 'output length');
   t.deepEqual(result, expect, 'output value');
 
   t.end();
