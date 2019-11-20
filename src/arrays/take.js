48edccd2ab9807b41c37e4f7bc51b8d5b7252d37
Evan Plaice <evanplaice@gmail.com>
2019-11-20 16:56:11 -0700

    Fix typo

--- src/arrays/take.js
+++ src/arrays/take.js
@@ -5,11 +5,11 @@
  * @param {Number} [len=1] optional number of elements in the slice of the array
  * @returns {Array} the slice of the array of length 'len'
  * @example
- * const result = arrays.some(['Amy', 'Brie', 'Cam', 'Dimitri']);
+ * const result = arrays.take(['Amy', 'Brie', 'Cam', 'Dimitri']);
  * console.log(result);
  * > ['Amy']
  * @example
- * const result = arrays.some(['Amy', 'Brie', 'Cam', 'Dimitri'], 3);
+ * const result = arrays.take(['Amy', 'Brie', 'Cam', 'Dimitri'], 3);
  * console.log(result);
  * > ['Amy', 'Brie', 'Cam']
  */
