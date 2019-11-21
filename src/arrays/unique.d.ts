a59fb4c31b49c04470a61706b7d76bc4e0a88f21
Evan Plaice <evanplaice@gmail.com>
2019-11-20 17:25:32 -0700

    Build

---
+++ src/arrays/unique.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Removes all duplicate items of an array
+ *
+ * @param {Array} array input array
+ * @returns {Array} an array of unique values
+ * @example
+ * const result = arrays.unique([2, 1, 2]);
+ * console.log(result);
+ * > [2, 1]
+ */
+export function unique(array: any[]): any[];
