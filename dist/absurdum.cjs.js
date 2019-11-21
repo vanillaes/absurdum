064d5ed5e24eeb0072089cb474ed720d57d98168
Evan Plaice <evanplaice@gmail.com>
2019-11-20 21:26:15 -0700

    Build

--- dist/absurdum.cjs.js
+++ dist/absurdum.cjs.js
@@ -390,6 +390,30 @@ function indexOf (array, searchElement, start = 0) {
 }
 
 /**
+ * Join takes an array and a seperator and returns a string of the array elements
+ * joined togeather by the seperator string
+ *
+ * @param {Array} array input array
+ * @param {String} [separator=','] optional string to separate each element of array
+ * @returns {String} String of array elements joined togeather by separtor string
+ *
+ * @example
+ * const result = arrays.join(['a','b','c','d']);
+ * console.log(result);
+ * > 'a,b,c,d'
+ * @example
+ * const result = arrays.join(['a','b','c','d'], 'x');
+ * console.log(result);
+ * > 'axbxcxd'
+ */
+function join (array, separator = ',') {
+  const len = array.length;
+  return array.reduce((acc, curr, idx) => {
+    return idx < len && idx > 0 ? acc + separator + curr : acc + curr;
+  }, '');
+}
+
+/**
  * Map iterates over an array of values and applies a function to each value
  *
  * @param {Array} array input array
@@ -643,6 +667,7 @@ var index = /*#__PURE__*/Object.freeze({
   flat: flat,
   fromPairs: fromPairs,
   indexOf: indexOf,
+  join: join,
   map: map,
   reduceRight: reduceRight,
   reverse: reverse,
---
+++ docs/arrays/join.md
@@ -0,0 +1,34 @@
+# arrays.join
+
+<!-- div class="doc-container" -->
+
+<!-- div -->
+
+
+<!-- div -->
+
+<h3 id="joinarray-separator-"><code>join(array, [separator=','])</code></h3>
+
+Join takes an array and a seperator and returns a string of the array elements
+joined togeather by the seperator string
+
+#### Arguments
+1. `array` *(Array)*: input array
+2. `[separator=',']` *(String)*: optional string to separate each element of array
+
+#### Returns
+*(String)*: String of array elements joined togeather by separtor string
+
+#### Example
+```js
+const result = arrays.join(['a','b','c','d']);
+console.log(result);
+> 'a,b,c,d'
+```
+---
+
+<!-- /div -->
+
+<!-- /div -->
+
+<!-- /div -->
