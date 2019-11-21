5525b447f9c0edfad9d70c3e3e3a85b6e5c6836a
Evan Plaice <evanplaice@gmail.com>
2019-11-21 16:09:55 -0700

    Build

---
+++ src/arrays/slice.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Take method returns a slice of array between input indexes start and end
+ *
+ * @param {Array} array input array
+ * @param {Number} [start=0] optional inclusive first index in the slice of the array
+ * @param {Number} [end=array.length] optional exclusive last index in the slice of the array
+ * @returns {Array} the slice of the array of between indexes start and end
+ * @example
+ * const result = arrays.slice(['Amy', 'Brie', 'Cam', 'Dimitri']);
+ * console.log(result);
+ * > ['Amy', 'Brie', 'Cam', 'Dimitri']
+ * @example
+ * const result = arrays.slice(['Amy', 'Brie', 'Cam', 'Dimitri'], 1, 3);
+ * console.log(result);
+ * > ['Brie', 'Cam']
+ */
+export function slice(array: any[], start?: number, end?: number): any[];
