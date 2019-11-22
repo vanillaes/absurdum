d8f7c83f9a866b035c61dbb77a0689ee00c993a1
Evan Plaice <evanplaice@gmail.com>
2019-11-21 23:57:49 -0700

    Build

--- dist/absurdum.esm.js
+++ dist/absurdum.esm.js
@@ -38,18 +38,7 @@ function chunk (array, size = 1) {
  */
 function compact (array) {
   return array.reduce((acc, curr) => {
-    if (isNaN(curr)) { return acc; }
-    switch (curr) {
-      case false:
-      case null:
-      case 0:
-      case '':
-      case undefined:
-        break;
-      default:
-        acc.push(curr);
-        break;
-    }
+    if (curr) { acc.push(curr); }
     return acc;
   }, []);
 }
