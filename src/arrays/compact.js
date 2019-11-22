f4b07993a0da4e054ccdfc39feb1c8bfac43073b
Evan Plaice <evanplaice@gmail.com>
2019-11-21 21:26:08 -0700

    simplify arrays.compact

--- src/arrays/compact.js
+++ src/arrays/compact.js
@@ -11,18 +11,7 @@
  */
 function compact (array) {
   return array.reduce((acc, curr) => {
-    if (isNaN(curr)) { return acc; }
-    switch (curr) {
-      case false:
-      case null:
-      case 0:
-      case '':
-      case undefined:
-        break;
-      default:
-        acc.push(curr);
-        break;
-    }
+    if (curr) { acc.push(curr); }
     return acc;
   }, []);
 }
