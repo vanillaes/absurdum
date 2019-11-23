9710cf4a5080cfe64980007fda58b275b3cec8cf
jkvyff <kristianvyff@gmail.com>
2019-11-23 10:52:34 -0800

    update arrays.flat to recursively implement spec

--- src/arrays/flat.spec.js
+++ src/arrays/flat.spec.js
@@ -23,9 +23,31 @@ test('arrays.flat(array) - an already flat array should return the same as the i
   t.end();
 });
 
-test('arrays.flat(array) - should flat multiple levels of nested arrays into one array', t => {
+test('arrays.flat(array) - should remove empty slots in arrays', t => {
+  const expect = [1, 3, 5];
+  const result = arrays.flat([1, , 3, , 5]);
+
+  t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
+  t.equal(result.length, 3, 'output length');
+  t.deepEqual(result, expect, 'output value');
+
+  t.end();
+});
+
+test('arrays.flat(array) - should flat multiple levels of nested arrays into one array as defined by depth', t => {
+  const expect = [1, 2, 3, [4]];
+  const result = arrays.flat([1, [2, [3, [4]]]], 2);
+
+  t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
+  t.equal(result.length, 4, 'output length');
+  t.deepEqual(result, expect, 'output value');
+
+  t.end();
+});
+
+test('arrays.flat(array) - should flat all levels of nested arrays into one array', t => {
   const expect = [1, 2, 3, 4];
-  const result = arrays.flat([1, [2, [3, [4]]]]);
+  const result = arrays.flat([1, [2, [3, [4]]]], Infinity);
 
   t.equal(Object.prototype.toString.call(result), '[object Array]', 'return type');
   t.equal(result.length, 4, 'output length');
