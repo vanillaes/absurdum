512a132b9c0249c4e4148302e34fba1cc392ae15
Evan Plaice <evanplaice@gmail.com>
2019-11-26 18:48:55 -0700

    Fix Types

--- src/arrays/lastIndexOf.js
+++ src/arrays/lastIndexOf.js
@@ -4,16 +4,16 @@
  *
  * @param {Array} array
  * @param {number} searchElement to be looked for in the array
- * @param {number} [end=array.length-1] index in array to begin searching for searchElement from right to left
+ * @param {number} [fromIndex=array.length-1] index in array to begin searching for searchElement from right to left
  * @returns {number} a integer representing the first index in the array that contains the element from right to left
  * @example
  * const result = arrays.lastIndexOf([1,2,3,4,5,4], 4, 4));
  * console.log(result);
  * > 3
  */
-function lastIndexOf (array, searchElement, last = array.length - 1) {
+function lastIndexOf (array, searchElement, fromIndex = array.length - 1) {
   return array.reduceRight((res, cur, i) => {
-    if (i <= last) {
+    if (i <= fromIndex) {
       return (res === -1 && cur === searchElement) ? i : res;
     } else {
       return -1;
