43c9373f76b426da7f28aafc404d869bcb35370f
Evan Plaice <evanplaice@gmail.com>
2019-12-12 00:55:47 -0700

    Fix Typings
    Rest params can't be optional.

--- src/objects/pick.d.ts
+++ src/objects/pick.d.ts
@@ -2,7 +2,7 @@
  * Pick returns a new object composed from the selected object properties.
  *
  * @param {object} object input object
- * @param {...(string|string[])} [paths] paths names of properties to be returned from an object
+ * @param {...(string|string[])} paths paths names of properties to be returned from an object
  * @returns {object} object with selected properties
  *
  * @example
@@ -10,4 +10,4 @@
  * console.log(result);
  * > { 45: 'pasta', a: 'mixed' }
  */
-export function pick(object: any, ...paths?: (string | string[])[]): any;
+export function pick(object: any, ...paths: (string | string[])[]): any;
--- src/objects/pick.js
+++ src/objects/pick.js
@@ -2,7 +2,7 @@
  * Pick returns a new object composed from the selected object properties.
  *
  * @param {object} object input object
- * @param {...(string|string[])} [paths] paths names of properties to be returned from an object
+ * @param {...(string|string[])} paths paths names of properties to be returned from an object
  * @returns {object} object with selected properties
  *
  * @example
