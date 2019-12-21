584a3d8ee962f0a045e894f1161bd1509d1bbe91
Evan Plaice <evanplaice@gmail.com>
2019-12-21 12:38:21 -0700

    Build

--- dist/absurdum.cjs
+++ dist/absurdum.cjs
@@ -739,6 +739,31 @@ function unique (array) {
 }
 
 /**
+ * Performs a manipulation to undo the zip command
+ *
+ * @param {Array} array input array
+ * @returns {Array} an array of unique values
+ * @example
+ * const result = arrays.unzip([['a', 'b', 'c'], [1, 2, 3], [true, false, true]]);
+ * console.log(result);
+ * > [['a', 1, true], ['b', 2, false], ['c', 3, true]]
+ */
+function unzip (array) {
+  const res = new Array(array.reduce((acc, curr) => {
+    return curr.length > acc ? curr.length : acc;
+  }, 0)).fill(undefined);
+
+  return array.reduce((acc, _, idx) => {
+    res.reduce((_, __, x) => {
+      acc[x] = acc[x] || [];
+      acc[x].push(array[idx][x]);
+      return _;
+    }, true);
+    return acc;
+  }, []);
+}
+
+/**
  * Zip applies a specified function to the corresponding elements of two sequences,
  * producing a sequence of the results.
  *
@@ -808,6 +833,7 @@ var index = /*#__PURE__*/Object.freeze({
   tap: tap,
   union: union,
   unique: unique,
+  unzip: unzip,
   zip: zip
 });
 
