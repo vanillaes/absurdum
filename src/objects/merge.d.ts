2676aa0a7b140b9e2f911434752253a0eb153982
Evan Plaice <evanplaice@gmail.com>
2019-12-30 12:29:51 -0700

    Rebuild Typings

--- src/objects/merge.d.ts
+++ src/objects/merge.d.ts
@@ -3,12 +3,12 @@
  * being merged recursively and other value types overridden when applied from left to right.
  *
  * @param {object} object input object
- * @param {...object} [sources] input object
- * @returns {array} returns an array of all values in an object
+ * @param {...object} [sources] input object/s
+ * @returns {object} returns an object with all included object properties merged
  *
  * @example
  * const result = objects.merge({ hold: 25, your: 19 }, { a: 1, b: 2 });
  * console.log(result);
  * > { a: 1, b: 2, hold: 25, your: 19 }
  */
-export function merge(object: any, ...sources?: any[]): any[];
+export function merge(object: any, ...sources?: any[]): any;
