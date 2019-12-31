df0d5c625d9aea81077e81bd502bf19796d23aad
Evan Plaice <evanplaice@gmail.com>
2019-12-31 16:07:25 -0700

    Build

--- dist/absurdum.esm.js
+++ dist/absurdum.esm.js
@@ -1337,6 +1337,43 @@ const objContained = (obj1, obj2) => {
 };
 
 /**
+ * Has, creates an array of values corresponding to paths of the object
+ *
+ * @param {object} object input object
+ * @param {Array|string} path strings describing paths to be returned from an object
+ * @returns {boolean} boolean true is a direct property of the object
+ *
+ * @example
+ * const result = objects.has({ front: [1, 3, 5], back: [37, 39] });
+ * console.log(result, 'back[1]');
+ * > true
+ *
+ * @example
+ * const result = objects.has({ front: [1, 3, 5], back: [37, 39] });
+ * console.log(result, ['front', 1]);
+ * > true
+ */
+function has (object, path) {
+  if (typeof path === 'undefined') { return false; }
+  let pathArray;
+
+  if (Array.isArray(path)) {
+    pathArray = path;
+  } else {
+    pathArray = String(path).replace(/\[(\w+)\]/g, '.$1').split('.');
+  }
+
+  return undefined !== pathArray.reduce((result, search) => {
+    if (result === undefined) { return undefined; }
+    if (Object.prototype.hasOwnProperty.call(result, search)) {
+      return result[search];
+    } else {
+      return undefined;
+    }
+  }, object);
+}
+
+/**
  * Include filters elements in a new object based on an array of keys to include
  *
  * @param {object} object input string
@@ -1526,6 +1563,7 @@ var index$2 = /*#__PURE__*/Object.freeze({
   defaults: defaults,
   exclude: exclude,
   findKey: findKey,
+  has: has,
   include: include,
   invert: invert,
   keys: keys,
