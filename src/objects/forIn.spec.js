8b38fc4e1a6a5c8dad2d2ab97afde71b4dc6f383
jkvyff <kristianvyff@gmail.com>
2020-01-02 17:24:46 -0800

    added testing for console.log

--- src/objects/forIn.spec.js
+++ src/objects/forIn.spec.js
@@ -1,6 +1,9 @@
 import test from 'tape';
 import { objects } from '../../index.js';
 
+const consoleLog = console.log;
+let logOutput = [];
+
 test('objects.forIn(object, func) - returns input object', t => {
   const expect = { a: 1, b: 2, c: 3 };
   const result = objects.forIn({ a: 1, b: 2, c: 3 }, (value) => { return value + 34; });
@@ -11,18 +14,36 @@ test('objects.forIn(object, func) - returns input object', t => {
   t.end();
 });
 
-test('objects.forIn(object, func) - returns input object', t => {
+test('objects.forIn(object, func) - returns input object and function return is ignored', t => {
   const expect = { a: 1, b: 2 };
   function TestObj () {
     this.a = 1;
     this.b = 2;
   }
   TestObj.prototype.c = 3;
-  const result = objects.forIn(new TestObj(), (value, key, object) => console.log(value, key, object));
+  const result = objects.forIn(new TestObj(), (value, key, object) => { return [value, key, object]; });
 
   t.equal(Object.prototype.toString.call(result), '[object Object]', 'return type');
   t.deepEqual(result, expect, 'output value');
 
+  t.end();
+});
+
+test('objects.forIn(object, func) - returns input object, logs to console as it iterates', t => {
+  const expect = ['1a', '2b', '3c'];
+  function TestObj () {
+    this.a = 1;
+    this.b = 2;
+  }
+  TestObj.prototype.c = 3;
+  console.log = e => { logOutput.push(e); };
+  const result = objects.forIn(new TestObj(), (value, key) => console.log(value + key));
+  console.log = consoleLog;
+
+  t.equal(Object.prototype.toString.call(result), '[object Object]', 'return type');
+  t.deepEqual(logOutput, expect, 'output value');
+
+  logOutput = [];
   t.end();
 });
 
