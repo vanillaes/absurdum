26864485ad66e061a9a5ac0a98ff4545b71a652d
Evan Plaice <evanplaice@gmail.com>
2020-01-05 14:07:14 -0700

    Build

--- dist/absurdum.esm.js
+++ dist/absurdum.esm.js
@@ -779,6 +779,37 @@ function without (array, ...values) {
 }
 
 /**
+ * Creates an array of unique values that is the symmetric difference of the given arrays
+ *
+ * @param {...Array} [arrays] input arrays
+ * @returns {Array} an array of unique values
+ * @example
+ * const result = arrays.xor(['a', 1, [5]], ['b', 1, 'a'], ['b', 'c', 5]);
+ * console.log(result);
+ * > [[5], 'c', 5]
+ */
+function xor (...arrays) {
+  if (arrays.length < 2) {
+    if (arrays.length === 0) { return []; }
+    return arrays[0];
+  }
+
+  const duplicates = new Set();
+  const allUniq = arrays.reduce((collect, array) => {
+    const checker = array.reduce((acc, curr) => {
+      if (collect.has(curr)) { acc.push(curr); }
+      return acc;
+    }, []);
+    checker.forEach(val => duplicates.add(val));
+    array.forEach(val => collect.add(val));
+    return collect;
+  }, new Set());
+
+  duplicates.forEach(val => allUniq.delete(val));
+  return [...allUniq];
+}
+
+/**
  * Zip applies a specified function to the corresponding elements of two sequences,
  * producing a sequence of the results.
  *
@@ -850,6 +881,7 @@ var index = /*#__PURE__*/Object.freeze({
   unique: unique,
   unzip: unzip,
   without: without,
+  xor: xor,
   zip: zip
 });
 
---
+++ docs/arrays/xor.md
@@ -0,0 +1,32 @@
+# arrays.xor
+
+<!-- div class="doc-container" -->
+
+<!-- div -->
+
+
+<!-- div -->
+
+<h3 id="xorarrays"><code>xor([arrays])</code></h3>
+
+Creates an array of unique values that is the symmetric difference of the given arrays
+
+#### Arguments
+1. `[arrays]` *(...Array)*: input arrays
+
+#### Returns
+*(Array)*: an array of unique values
+
+#### Example
+```js
+const result = arrays.xor(['a', 1, [5]], ['b', 1, 'a'], ['b', 'c', 5]);
+console.log(result);
+> [[5], 'c', 5]
+```
+---
+
+<!-- /div -->
+
+<!-- /div -->
+
+<!-- /div -->
