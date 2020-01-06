1dcab0ef3993748cd648a710abf55b67ddf9faeb
jkvyff <kristianvyff@gmail.com>
2020-01-06 01:18:00 -0800

    remove overloading to preserve SRP

--- src/objects/transform.js
+++ src/objects/transform.js
@@ -3,7 +3,7 @@
  *
  * @param {object} object input object
  * @param {function} [func] iteratee function
- * @param {object|Array} [accumulator] custom accumulator object
+ * @param {object|Array} [accumulator = {}] custom accumulator object
  * @returns {object|Array} returns accumulator object after the input object has been iterated over by the function.
  *
  * @example
@@ -12,32 +12,14 @@
  * });
  * console.log(result);
  * > { harmony: '7_0', daft: '9_1', stripes: '11_2' }
- *
- * const result = objects.transform([2, 3, 4], (acc, val, idx) => {
- *   acc[idx] = val + 5;
- *   return n % 2 == 0;
- * });
- * console.log(result);
- * > [7, 8]
  */
-function transform (object, func, accumulator) {
+function transform (object, func, accumulator = {}) {
   if (typeof func === 'undefined') { return object; }
-  const isArray = Array.isArray(object);
-  if (typeof accumulator === 'undefined') {
-    accumulator = isArray ? [] : {};
-  }
 
-  if (isArray) {
-    return object.reduce((acc, value, index, arr) => {
-      if (func(acc, value, index, arr) === false) { arr.splice(0); }
-      return acc;
-    }, accumulator);
-  } else {
-    return Object.entries(object).reduce((acc, [key, value], _, arr) => {
-      if (func(acc, value, key, object) === false) { arr.splice(0); }
-      return acc;
-    }, accumulator);
-  }
+  return Object.entries(object).reduce((acc, [key, value]) => {
+    func(acc, value, key, object);
+    return acc;
+  }, accumulator);
 }
 
 export { transform };
