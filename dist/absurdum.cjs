d7600216d4aa70b24d7b001cf06ff94400dcfd1f
Evan Plaice <evanplaice@gmail.com>
2020-01-09 16:32:40 -0700

    Build

--- dist/absurdum.cjs
+++ dist/absurdum.cjs
@@ -1266,6 +1266,32 @@ function reverse$1 (string) {
 }
 
 /**
+ * snakeCase updates a string to snakecase
+ *
+ * @param {string} [string=''] input string
+ * @returns {string} returns new snakeCase string
+ *
+ * @example
+ * const result = strings.snakeCase('--BEST--friend--');
+ * console.log(result);
+ * > 'best_friend'
+ */
+function snakeCase (string = '') {
+  let first = true;
+  return string.replace(/[\u2019']/, '').split(/[\u002D\u2014\-_\s]+/).reduce((acc, word) => {
+    if (word.length > 0) {
+      if (first) {
+        first = false;
+        return word.toLowerCase();
+      } else {
+        return acc + '_' + word.toLowerCase();
+      }
+    }
+    return acc;
+  }, '');
+}
+
+/**
  * StartsWith tests a string to see if it starts with a substring
  *
  * @param {string} string input string
@@ -1317,6 +1343,7 @@ var index$1 = /*#__PURE__*/Object.freeze({
   pascalCase: pascalCase,
   repeat: repeat,
   reverse: reverse$1,
+  snakeCase: snakeCase,
   startsWith: startsWith
 });
 
