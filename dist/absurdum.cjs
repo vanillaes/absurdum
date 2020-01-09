32cfe387545bccaa108494ce623b99ff9550e6ff
Evan Plaice <evanplaice@gmail.com>
2020-01-08 17:21:20 -0700

    Build

--- dist/absurdum.cjs
+++ dist/absurdum.cjs
@@ -890,6 +890,31 @@ var index = /*#__PURE__*/Object.freeze({
 });
 
 /**
+ * camelCase updates a string to camelcase
+ *
+ * @param {string} [string=''] input string
+ * @returns {string} returns new camelCase string
+ *
+ * @example
+ * const result = strings.camelCase('--BEST_friend--');
+ * console.log(result);
+ * > 'bestFriend'
+ */
+function camelCase (string = '') {
+  let first = true;
+  const res = string.replace(/[\u2019']/, '').split(/[\u002D\u2014\-_\s]+/).reduce((acc, word) => {
+    if (first && word.length > 0) {
+      word = word.toLowerCase();
+      first = false;
+    } else {
+      word = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }
+    return acc + word;
+  }, '');
+  return res;
+}
+
+/**
  * Chomp removes record separator characters (ex \n, \r, \r\n) from the end of a string.
  *
  * @param {string} string input string
@@ -1231,6 +1256,7 @@ function startsWith (string, substr) {
 
 var index$1 = /*#__PURE__*/Object.freeze({
   __proto__: null,
+  camelCase: camelCase,
   chomp: chomp,
   endsWith: endsWith,
   includes: includes,
