b438f5ed6b3d4892f8c92233a7efabe64e6bede9
Evan Plaice <evanplaice@gmail.com>
2020-01-08 17:26:32 -0700

    Build

--- dist/absurdum.esm.js
+++ dist/absurdum.esm.js
@@ -1058,6 +1058,38 @@ function indexOf$1 (string, substr, start = 0) {
 }
 
 /**
+ * Pads the both ends of a string w/ repeated spaces|substrings
+ *
+ * @param {string} string input string
+ * @param {number} length length of the padded portion
+ * @param {string} [substr=' '] substring to apply
+ * @returns {string} the input padded w/ spaces|substrings
+ *
+ * @example
+ * // if no `substr` is provided, it pads the string w/ spaces
+ * const result = strings.pad('xyzxyz', 9);
+ * console.log(result);
+ * > ' xyzxyz  '
+ *
+ * @example
+ * // if `length` is shorter than `string` it doesn't add any padding
+ * const result = strings.pad('xyzxyz', 4);
+ * console.log(result);
+ * > 'xyzxyz'
+ *
+ * @example
+ * // if `substr` is defined, it uses that for padding
+ *  const result = strings.pad('xyzxyz', 16, 'FUN');
+ *  console.log(result);
+ *  > 'FUNFUxyzxyzFUNFU'
+ */
+function pad (string, length, substr = ' ') {
+  const strLen = string.length;
+  const padLen = (length - strLen) > 0 ? Math.ceil((length - strLen) / 2) : 0;
+  return string.padStart(length - padLen, substr).padEnd(length, substr);
+}
+
+/**
  * Pads the end of a string w/ repeated spaces|substrings
  *
  * @param {string} string input string
@@ -1231,6 +1263,7 @@ var index$1 = /*#__PURE__*/Object.freeze({
   endsWith: endsWith,
   includes: includes,
   indexOf: indexOf$1,
+  pad: pad,
   padEnd: padEnd,
   padStart: padStart,
   repeat: repeat,
