edd7f1e307edda240e4186bda0d8f32b44690027
jkvyff <kristianvyff@gmail.com>
2020-01-12 07:31:03 -0800

    deburr method, docs, tests

---
+++ src/strings/deburr.spec.js
@@ -0,0 +1,42 @@
+import test from 'tape';
+import { strings } from '../../index.js';
+
+test('strings.deburr(string) - should return the string converting to basic Latin letters', t => {
+  const expect = 'aeNaThsЄL,';
+  const result = strings.deburr('æŅåÞşЄŁ,');
+
+  t.equal(Object.prototype.toString.call(result), '[object String]', 'return type');
+  t.equal(result, expect, 'output value');
+
+  t.end();
+});
+
+test('strings.deburr(string) - should return the string converting to basic Latin letters', t => {
+  const expect = 'deja vu';
+  const result = strings.deburr('déjà vu');
+
+  t.equal(Object.prototype.toString.call(result), '[object String]', 'return type');
+  t.equal(result, expect, 'output value');
+
+  t.end();
+});
+
+test('strings.deburr(string) - should return the string with combining diacritical marks', t => {
+  const expect = 'troll bin';
+  const result = strings.deburr('tro\u0311ll bin\u0338');
+
+  t.equal(Object.prototype.toString.call(result), '[object String]', 'return type');
+  t.equal(result, expect, 'output value');
+
+  t.end();
+});
+
+test('strings.deburr(string) - should return empty string if provided an empty string', t => {
+  const expect = '';
+  const result = strings.deburr('');
+
+  t.equal(Object.prototype.toString.call(result), '[object String]', 'return type');
+  t.equal(result, expect, 'output value');
+
+  t.end();
+});
