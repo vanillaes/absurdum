4903066946465fe16d365d9d2274888d167b6997
Evan Plaice <evanplaice@gmail.com>
2020-01-12 23:37:55 -0700

    Build

--- dist/absurdum.cjs
+++ dist/absurdum.cjs
@@ -1169,6 +1169,35 @@ function trimStart (string = '', chars = ' ') {
   }, '');
 }
 
+/**
+ * TrimEnd trims any whitespace or the selected characters from the end of the string
+ *
+ * @param {string} [string=''] input string
+ * @param {string} [chars=' '] characters to remove from end of the string
+ * @returns {string} string with the characters removed from end of the string
+ *
+ * @example
+ * const result = strings.trimEnd('-_-abc-_-', '_-');
+ * console.log(result);
+ * > '-_-abc'
+ */
+function trimEnd (string = '', chars = ' ') {
+  const testChars = new Set([...chars]);
+  let end = true;
+  return [...string].reduceRight((acc, curr) => {
+    if (end) {
+      if (testChars.has(curr)) {
+        return acc;
+      } else {
+        end = false;
+        return curr + acc;
+      }
+    } else {
+      return curr + acc;
+    }
+  }, '');
+}
+
 
 
 var index$1 = /*#__PURE__*/Object.freeze({
@@ -1187,7 +1216,8 @@ var index$1 = /*#__PURE__*/Object.freeze({
   reverse: reverse,
   snakeCase: snakeCase,
   startsWith: startsWith,
-  trimStart: trimStart
+  trimStart: trimStart,
+  trimEnd: trimEnd
 });
 
 /**
---
+++ docs/strings/trimEnd.md
@@ -0,0 +1,33 @@
+# strings.trimEnd
+
+<!-- div class="doc-container" -->
+
+<!-- div -->
+
+
+<!-- div -->
+
+<h3 id="trimendstring-chars"><code>trimEnd([string=''], [chars=' '])</code></h3>
+
+TrimEnd trims any whitespace or the selected characters from the end of the string
+
+#### Arguments
+1. `[string='']` *(string)*: input string
+2. `[chars=' ']` *(string)*: characters to remove from end of the string
+
+#### Returns
+*(string)*: string with the characters removed from end of the string
+
+#### Example
+```js
+const result = strings.trimEnd('-_-abc-_-', '_-');
+console.log(result);
+> '-_-abc'
+```
+---
+
+<!-- /div -->
+
+<!-- /div -->
+
+<!-- /div -->
