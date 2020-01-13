d383ef1d40f4e5916244201a5e462230229358ed
Evan Plaice <evanplaice@gmail.com>
2020-01-12 23:37:55 -0700

    Build

---
+++ src/strings/trimEnd.d.ts
@@ -0,0 +1,13 @@
+/**
+ * TrimEnd trims any whitespace or the selected characters from the end of the string
+ *
+ * @param {string} [string=''] input string
+ * @param {string} [chars=' '] characters to remove from end of the string
+ * @returns {string} string with the characters removed from end of the string
+ *
+ * @example
+ * const result = strings.trimEnd('-_-abc-_-', '_-');
+ * console.log(result);
+ * > '-_-abc'
+ */
+export function trimEnd(string?: string, chars?: string): string;
