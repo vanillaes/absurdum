679a6e0da930a7280d9f6a7859036988484d2758
Evan Plaice <evanplaice@gmail.com>
2020-01-13 21:09:01 -0700

    Build

---
+++ src/strings/words.d.ts
@@ -0,0 +1,23 @@
+/**
+ * Splits `string` into an array of its words.
+ *
+ * @param {string} [string=''] string to inspect for words
+ * @param {RegExp|string} [pattern] regex pattern to match words or string of characters to split words by.
+ * @returns {Array} Returns an array of words
+ *
+ * @example
+ * const result = words('I can, I should, & I will');
+ * console.log(result);
+ * // => ['I', 'can', 'I', 'should', 'I', 'will']
+ *
+ * @example
+ * const result = words('I can, I should, & I will', ' ,');
+ * console.log(result);
+ * // => ['I', 'can', 'I', 'should', '&', 'I', 'will']
+ *
+ * @example
+ * const result = words('I can, I should, & I will', /[^, ]+/g);
+ * console.log(result);
+ * // => ['I', 'can', 'I', 'should', '&', 'I', 'will']
+ */
+export function words(string?: string, pattern?: string | RegExp): any[];
