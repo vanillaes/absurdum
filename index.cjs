cfe6c19d672f52c25adc239b2d32cb8b8034b48c
Evan Plaice <evanplaice@gmail.com>
2020-01-19 16:19:12 -0700

    Build

--- index.cjs
+++ index.cjs
@@ -1852,16 +1852,16 @@ const arrayMerge$1 = (current, source) => {
 const objectMerge$1 = (current, source) => {
   const res = {};
   if (typeof current === 'object') {
-    Object.entries(current).reduce((_, entry) => {
-      res[entry[0]] = entry[1];
+    Object.keys(current).reduce((_, key) => {
+      res[key] = current[key];
       return null;
     }, null);
   }
-  Object.entries(source).reduce((_, entry) => {
-    if (typeof entry[1] !== 'object' || !current[entry[0]]) {
-      res[entry[0]] = entry[1];
+  Object.keys(source).reduce((_, key) => {
+    if (typeof source[key] !== 'object' || !current[key]) {
+      res[key] = source[key];
     } else {
-      res[entry[0]] = deepMerge$1(current[entry[0]], entry[1]);
+      res[key] = deepMerge$1(current[key], source[key]);
     }
     return null;
   }, null);
