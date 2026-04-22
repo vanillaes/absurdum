b721382dbb6490ae72f6181d6bde64cec5248382
Evan Plaice <evanplaice@gmail.com>
2026-04-22 16:11:25 -0600

    Fix API

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 /* @ts-self-types="./index.d.ts" */
-
-import * as arrays from './src/arrays/index.js'
-import * as strings from './src/strings/index.js'
-import * as objects from './src/objects/index.js'
-
-export { arrays, strings, objects }
+export * as arrays from './src/arrays/index.js'
+export * as strings from './src/strings/index.js'
+export * as objects from './src/objects/index.js'
